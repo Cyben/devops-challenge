3e2689b786e0fcf11b4cc2265ba8340cc6e5caea
Cyben <benshaver8@gmail.com>
2025-07-02 14:03:57 +0300

    fixed snake_case to camelCase

--- thedoctor/app/main.py
+++ thedoctor/app/main.py
@@ -1,10 +1,25 @@
 from flask import Flask, jsonify
 from dotenv import load_dotenv
-from app.dynamo_db import get_secret
+import boto3
+import os
 
 load_dotenv()
 
 app = Flask(__name__)
+
+def get_secret():
+    code_name = os.getenv("CODE_NAME", "theDoctor")
+    table_name = "devops-challenge"
+
+    dynamodb = boto3.resource(
+        'dynamodb',
+        aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+        region_name=os.getenv("AWS_REGION", "eu-west-1")
+    )
+    table = dynamodb.Table(table_name)
+    response = table.get_item(Key={"codeName": code_name})
+    return response['Item'].get('secretCode', 'NOT_FOUND')
 
 @app.route('/secret', methods=['GET'])
 def secret():
