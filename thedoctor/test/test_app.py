bd5b80ee0c949d509f0fec116f29193b41f6f95e
Cyben <benshaver8@gmail.com>
2025-07-02 14:04:18 +0300

    added secret endpoint test

--- thedoctor/test/test_app.py
+++ thedoctor/test/test_app.py
@@ -1,9 +1,9 @@
 import pytest
-from app.main import app
+import app.main
 
 @pytest.fixture
 def client():
-    with app.test_client() as client:
+    with app.main.app.test_client() as client:
         yield client
 
 def test_health(client):
@@ -14,21 +14,21 @@ def test_health(client):
     assert 'container' in data
     assert 'project' in data
 
-# def test_secret_success(monkeypatch, client):
-#     # Mock boto3.resource().Table().get_item()
-#     class MockTable:
-#         def get_item(self, Key):
-#             return {'Item': {'secret_code': 'supersecret'}}
-#     class MockDynamoDB:
-#         def Table(self, name):
-#             return MockTable()
+def test_secret_success(monkeypatch, client):
+    # Mock boto3.resource().Table().get_item()
+    class MockTable:
+        def get_item(self, Key):
+            return {'Item': {'secretCode': 'supersecret'}}
+    class MockDynamoDB:
+        def Table(self, name):
+            return MockTable()
 
-#     def mock_boto3_resource(*args, **kwargs):
-#         return MockDynamoDB()
+    def mock_boto3_resource(*args, **kwargs):
+        return MockDynamoDB()
 
-#     monkeypatch.setattr('app.main.boto3.resource', mock_boto3_resource)
+    monkeypatch.setattr('app.main.boto3.resource', mock_boto3_resource)
 
-#     rv = client.get('/secret')
-#     assert rv.status_code == 200
-#     data = rv.get_json()
-#     assert data['secret_code'] == 'supersecret'
+    rv = client.get('/secret')
+    assert rv.status_code == 200
+    data = rv.get_json()
+    assert data['secret_code'] == 'supersecret'
