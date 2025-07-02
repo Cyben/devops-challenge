fc02a15c05c917849e9fdc737a15dec9663c7bb1
Cyben <benshaver8@gmail.com>
2025-07-02 11:44:34 +0300

    thedoctor app implementation

---
+++ thedoctor/app/main.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+from dotenv import load_dotenv
+from dynamo_db import get_secret
+
+load_dotenv()
+
+app = Flask(__name__)
+
+@app.route('/secret', methods=['GET'])
+def secret():
+    return jsonify({"secret_code": get_secret()})
+
+@app.route('/health', methods=['GET'])
+def health():
+    return jsonify({
+        "status": "healthy",
+        "container": "https://hub.docker.com/r/cybencyben/thedoctor-app",
+        "project": "https://github.com/Cyben/devops-challenge"
+    })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
